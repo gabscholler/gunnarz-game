a0a2cf570b6deaff9becc5b61f76ca7d3bdc3d34
root <root@red-worried-bluebell.7d2d4a8a.katapult.cloud>
2024-08-04 14:56:59 +0100

    Re-added public directory with index.html and game.js

---
+++ public/game.js
@@ -0,0 +1,97 @@
+const canvas = document.getElementById('gameCanvas');
+const context = canvas.getContext('2d');
+const socket = io();
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const players = {};
+const playerRadius = 20;
+const moveSpeed = 5;
+
+socket.on('connect', () => {
+  console.log('Connected to server');
+});
+
+socket.on('currentPlayers', (currentPlayers) => {
+  Object.keys(currentPlayers).forEach((id) => {
+    if (!players[id]) {
+      players[id] = currentPlayers[id];
+    }
+  });
+  draw();
+});
+
+socket.on('newPlayer', (newPlayer) => {
+  players[newPlayer.id] = { x: newPlayer.x, y: newPlayer.y, color: newPlayer.color };
+  draw();
+});
+
+socket.on('move', (data) => {
+  if (players[data.id]) {
+    players[data.id].x = data.x;
+    players[data.id].y = data.y;
+    draw();
+  }
+});
+
+socket.on('remove', (data) => {
+  delete players[data.id];
+  draw();
+});
+
+const keys = {};
+
+window.addEventListener('keydown', (e) => {
+  keys[e.key] = true;
+});
+
+window.addEventListener('keyup', (e) => {
+  keys[e.key] = false;
+});
+
+function update() {
+  if (players[socket.id]) {
+    let player = players[socket.id];
+    if (keys['w'] && player.y - playerRadius - moveSpeed >= 0) player.y -= moveSpeed;
+    if (keys['a'] && player.x - playerRadius - moveSpeed >= 0) player.x -= moveSpeed;
+    if (keys['s'] && player.y + playerRadius + moveSpeed <= canvas.height) player.y += moveSpeed;
+    if (keys['d'] && player.x + playerRadius + moveSpeed <= canvas.width) player.x += moveSpeed;
+
+    for (let id in players) {
+      if (id !== socket.id) {
+        let otherPlayer = players[id];
+        let dx = player.x - otherPlayer.x;
+        let dy = player.y - otherPlayer.y;
+        let distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < playerRadius * 2) {
+          if (keys['w']) player.y += moveSpeed;
+          if (keys['a']) player.x += moveSpeed;
+          if (keys['s']) player.y -= moveSpeed;
+          if (keys['d']) player.x -= moveSpeed;
+        }
+      }
+    }
+
+    socket.emit('move', { x: player.x, y: player.y });
+  }
+}
+
+function draw() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  for (let id in players) {
+    context.beginPath();
+    context.arc(players[id].x, players[id].y, playerRadius, 0, Math.PI * 2);
+    context.fillStyle = players[id].color;
+    context.fill();
+  }
+}
+
+function gameLoop() {
+  update();
+  draw();
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
+
